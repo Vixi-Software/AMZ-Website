e3e3c6f54b99b164a855755677027ce38f99d8a4
trannhon2509 <nhonvn2509@gmail.com>
2025-06-22 16:10:07 +0700

    reducr time

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -6,7 +6,7 @@ import { db } from "../utils/firebase";
 const collectionName = "postService";
 const PAGE_SIZE = 10;
 const ORDER_FIELD = "createdAt";
-const TIME_EXPIRATION = 30 * 60 * 1000; 
+const TIME_EXPIRATION = 30 * 1000; 
 const LAST_FETCHED_KEY = "posts_last_fetched";
 
 export const usePostService = () => {
